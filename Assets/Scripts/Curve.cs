13e51a2cb2d80b14a2db7c2f4b45cd0c787c57b2
wtlllll190812 <3200102777@zju.edu.cn>
2022-07-16 23:08:32 +0800

    渲染部分完成

---
+++ Assets/Scripts/Curve.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Curve : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public Transform pos;
+    public bool canMove=true;
+    public Vector3[] normals;
+    public Vector3[] vertices;
+    void Start()
+    {
+        cal();
+        StartCoroutine(Move());
+    }
+
+    private void cal()
+    {
+        vertices = meshFilter.sharedMesh.vertices;
+        normals = meshFilter.sharedMesh.normals;
+        var meshTr = meshFilter.transform;
+        //计算向量
+        int[] triangles = meshFilter.sharedMesh.triangles;
+        int triangelCount = triangles.Length / 3;
+        for (int i = 0; i < triangelCount; i++)
+        {
+            int index = i * 3;
+
+            Vector3 vertext_start = meshTr.TransformPoint(vertices[triangles[index]]);
+            Vector3 vertext_middle = meshTr.TransformPoint(vertices[triangles[index + 1]]);
+            Vector3 vertext_end = meshTr.TransformPoint(vertices[triangles[index + 2]]);
+
+            Vector3 startToMiddle = vertext_middle - vertext_start;
+            Vector3 middleToEnd = vertext_end - vertext_middle;
+
+            Vector3 triangleCenter = Vector3.zero;
+            triangleCenter.x = (vertext_start.x + vertext_middle.x + vertext_end.x) / 3.0f;
+            triangleCenter.z = (vertext_start.z + vertext_middle.z + vertext_end.z) / 3.0f;
+            triangleCenter.y = (vertext_start.y + vertext_middle.y + vertext_end.y) / 3.0f;
+
+            Vector3 normal = Vector3.Cross(startToMiddle, middleToEnd).normalized;
+
+            normals[triangles[index]] = normal;
+            normals[triangles[index + 1]] = normal;
+            normals[triangles[index + 2]] = normal;
+            vertices[triangles[index]] = vertext_start;
+            vertices[triangles[index + 1]] = vertext_middle;
+            vertices[triangles[index + 2]] = vertext_end;
+        }
+    }
+    
+    private IEnumerator Move()
+    {
+        int count = vertices.Length -1;
+        while (canMove)
+        {
+            if(count<=0)
+                count = vertices.Length - 1;
+            //transform.position = vertices[count];
+            //transform.LookAt(pos.position, normals[count]);
+            Debug.DrawRay(vertices[count], normals[count]);
+            //Debug.Log(normals[count]);
+            count -=2;
+            //yield return null;// new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+    //void OnDrawGizmos()
+    //{
+    //    if (!caled)
+    //    {
+    //        caled = true;
+    //        cal();
+    //    }
+    //    Gizmos.color = Color.yellow;
+    //    for (int i = 0; i < vertices.Length; i += 2)
+    //    {
+    //        Gizmos.DrawRay(vertices[i], normals[i]);
+    //    }
+    //}
+}
---
+++ Assets/assets/Materials/OutLine.shader
@@ -0,0 +1,57 @@
+﻿Shader "Unlit/Outline"
+{
+    Properties
+    {
+        _OutlineColor("Outline Color",Color) = (0,0,0,1)
+        _Outline ("Outline",Range(0,1)) = 0.1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "RenderPipeline"="UniversalRenderPipeline"}
+        LOD 100
+        Cull Front
+
+        pass
+        {
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            
+            
+            CBUFFER_START(UnityPerMaterial)         
+                float4 _OutlineColor;
+                float _Outline;
+            CBUFFER_END
+            
+            struct Atributes
+            {
+                float4 positionOS : POSITION;
+                float3 normalOS : NORMAL;
+            };
+
+            struct Varyings
+            {
+                float4 positionCS : SV_POSITION;
+            };
+            Varyings vert (Atributes v)
+            {
+                Varyings o = (Varyings)0;
+                o.positionCS = TransformObjectToHClip(v.positionOS);
+                //法线转到屏幕坐标
+                float3 vNormal = normalize(mul((float3x3)UNITY_MATRIX_IT_MV, v.normalOS));
+                //再转到裁切坐标
+                float2 projPos = normalize(mul((float2x2)UNITY_MATRIX_P,vNormal.xy));
+                
+                o.positionCS.xy += projPos * _Outline * 0.1;
+                return o;
+            }
+
+            real4 frag (Varyings i) : SV_Target
+            {
+                return _OutlineColor;
+            }
+            ENDHLSL
+        }
+    }
+}
